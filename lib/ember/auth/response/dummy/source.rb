heartsentwined/ember-auth-session-cookie

ruby source gem api

lib/ember/auth/session/cookie/source.rb
@@ -1,0 +1,15 @@
+module Ember
+  module Auth
+    module Session
+      module Cookie
+        module Source
+          def self.bundled_path
+            path = File.join '..', '..', '..', '..', '..', '..', \
+              'dist', 'ember-auth-session-cookie.js'
+            File.expand_path path, __FILE__
+          end
+        end
+      end
+    end
+  end
+end

heartsentwined/ember-auth-response-dummy

lib/ember/auth/response/dummy/source.rb
@@ -1,0 +1,15 @@
+module Ember
+  module Auth
+    module Response
+      module Dummy
+        module Source
+          def self.bundled_path
+            path = File.join '..', '..', '..', '..', '..', '..', \
+              'dist', 'ember-auth-response-dummy.js'
+            File.expand_path path, __FILE__
+          end
+        end
+      end
+    end
+  end
+end